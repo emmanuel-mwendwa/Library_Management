1a31dbb45a9e295f20809ac8c033ae91c4066016
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-04-19 12:35:57 +0300

    Server side and client side scripting and API call

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Emmanuel Mwendwa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Article", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Article", {
+  refresh: function (frm) {
+    frm.add_custom_button("Create Transaction", () => {
+      frappe.new_doc("Library Transaction", {
+        article: frm.doc.name,
+      });
+    });
+  },
+});
